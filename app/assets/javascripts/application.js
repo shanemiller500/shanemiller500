4bf8fd084e5b5a20ea33ab5f213e5652d6c3a9cd
shane <shanemiller500@gmail.com>
2014-06-19 19:30:39 -0700

    fixed scroller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,19 +28,19 @@ var section_show_time = 1300;
 // jQuery stuff
 jQuery(document).ready(function($) {
 
-  // Switch section
-  $("a", '.mainmenu').click(function() 
-  {
-    if( ! $(this).hasClass('active') ) { 
-      current_item = this;
-      // close all visible divs with the class of .section
-      $('.section:visible').fadeOut( section_hide_time, function() { 
-        $('a', '.mainmenu').removeClass( 'active' );  
-        $(current_item).addClass( 'active' );
-        var new_section = $( $(current_item).attr('href') );
-        new_section.fadeIn( section_show_time );
-      } );
-    }
-    return false;
-  });   
+// Switch section
+$("a", '.mainmenu').click(function() 
+{
+  if( ! $(this).hasClass('active') ) { 
+    current_item = this;
+// close all visible divs with the class of .section
+$('.section:visible').fadeOut( section_hide_time, function() { 
+  $('a', '.mainmenu').removeClass( 'active' );  
+  $(current_item).addClass( 'active' );
+  var new_section = $( $(current_item).attr('href') );
+  new_section.fadeIn( section_show_time );
+} );
+}
+return false;
+});   
 });
